66e155e01316111d4ac4720545b5b3b7003486b1
Varun Chopra <varunchopra2003@gmail.com>
2024-04-20 02:41:10 +0530

    Distance Functionality Added

--- model_data/Detector.py
+++ model_data/Detector.py
@@ -1,9 +1,9 @@
 import cv2
-
 import numpy as np
 import time
 
 np.random.seed(20)
+
 class Detector:
     def __init__(self, videoPath, configPath, modelPath, classesPath):
         self.videoPath = videoPath
@@ -18,7 +18,8 @@ class Detector:
         self.net.setInputSwapRB(True)
 
         self.readClasses()
-    
+        self.known_width = 10
+        self.focal_length = 280
 
     def readClasses(self):
         with open(self.classesPath, 'r') as f:
@@ -26,15 +27,17 @@ class Detector:
 
         self.classesList.insert(0, '__Background__')
         self.colorList = np.random.uniform(low=0, high=255, size=(len(self.classesList),3))
-    
+
+    def calculate_distance(self, known_width, focal_length, per_width):
+        return (known_width * focal_length) / per_width
 
     def onVideo(self):
         cap = cv2.VideoCapture(self.videoPath)
 
-        if(cap.isOpened()==False):
+        if not cap.isOpened():
             print("Error Opening Video File...")
             return
-        
+
         (success, image) = cap.read()
 
         while success:
@@ -48,39 +51,30 @@ class Detector:
 
             if len(bboxIdx) != 0:
                 for i in range(0, len(bboxIdx)):
-                    bbox = bboxs [np.squeeze(bboxIdx[i])]
+                    bbox = bboxs[np.squeeze(bboxIdx[i])]
                     classConfidence = confidences[np.squeeze(bboxIdx[i])]
-                    classLabelID = np. squeeze(classLabelIDs[np.squeeze(bboxIdx[i])])
+                    classLabelID = np.squeeze(classLabelIDs[np.squeeze(bboxIdx[i])])
                     classLabel = self.classesList[classLabelID]
-
-                    # bbox = bboxs [np.squeeze(bboxIdx[i])]
-                    # classConfidence = confidences[np.squeeze(bboxIdx[i])]
 
                     classColor = [int(c) for c in self.colorList[classLabelID]]
 
                     displayText = "{}:{:.2f}".format(classLabel, classConfidence)
-                    
-                    x,y,w,h = bbox
 
-                    cv2.rectangle(image, (x,y), (x+w, y+h), color=classColor, thickness=1)
-                    cv2.putText(image, displayText, (x, y-10), cv2. FONT_HERSHEY_PLAIN, 1, classColor, 2)
+                    x, y, w, h = bbox
 
+                    cv2.rectangle(image, (x, y), (x + w, y + h), color=classColor, thickness=1)
+                    cv2.putText(image, displayText, (x, y - 10), cv2.FONT_HERSHEY_PLAIN, 1, classColor, 2)
 
-                    #####################
+                    object_width = w
+                    distance = self.calculate_distance(known_width=10, focal_length=self.focal_length, per_width=object_width)
+                    cv2.putText(image, f"Distance: {distance:.2f} cm", (x, y + h + 20), cv2.FONT_HERSHEY_PLAIN, 1, classColor, 2)
 
-
-                    lineWidth = min(int(w * 0.3), int(h * 0.3))
-                    cv2.line(image, (x,y), (x + lineWidth, y), classColor, thickness=5)
-                    cv2.line(image, (x,y), (x, y + lineWidth), classColor, thickness=5)
-
-                    cv2.line(image, (x + w,y), (x + w - lineWidth, y), classColor, thickness= 5)
-                    cv2.line(image, (x + w,y), (x + w, y + lineWidth), classColor, thickness= 5)
-
-            cv2. imshow( "Result", image)
+            cv2.imshow("Result", image)
 
             key = cv2.waitKey(1) & 0xFF
             if key == ord("q"):
                 break
-            
-            (success, image) = cap. read()
+
+            (success, image) = cap.read()
+
         cv2.destroyAllWindows()
