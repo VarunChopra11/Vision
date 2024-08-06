30a7a1d80d517e825a734bdb5d9076fd2b5e2eed
Varun Chopra <varunchopra2003@gmail.com>
2024-08-06 22:44:17 +0530

    Update

--- model_data/main.py
+++ model_data/main.py
@@ -3,9 +3,12 @@ import os
 
 def main():
     server_address = "http://192.168.1.1/"
-    configPath = os.path.join("model_data", "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt")
-    modelPath = os.path.join("model_data", "frozen_inference_graph.pb")
-    classesPath = os.path.join("model_data", "coco.names")
+    configPath = "\\Your\\ssd_mobilenet\\file\\path"
+    modelPath = "\\Your\\frozen_inference\\file\\path"
+    classesPath = "\\Your\\coco.names\\file\\path"
+    # configPath = os.path.join("model_data", "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt")
+    # modelPath = os.path.join("model_data", "frozen_inference_graph.pb")
+    # classesPath = os.path.join("model_data", "coco.names")
     focalLength = 367  # or whatever the correct value is for your setup
 
     detector = Detector(server_address, configPath, modelPath, classesPath, focalLength)
