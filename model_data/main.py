d74dbd124802e3087cfe713132d73e0fd76caeeb
Varun Chopra <varunchopra2003@gmail.com>
2024-11-28 15:03:32 +0530

    Audio Detection Feat Added.

--- model_data/main.py
+++ model_data/main.py
@@ -2,16 +2,25 @@ from Detector import *
 import os
 
 def main():
-    server_address = "http://192.168.1.1/"
-    configPath = "\\Your\\ssd_mobilenet\\file\\path"
-    modelPath = "\\Your\\frozen_inference\\file\\path"
-    classesPath = "\\Your\\coco.names\\file\\path"
-    # configPath = os.path.join("model_data", "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt")
-    # modelPath = os.path.join("model_data", "frozen_inference_graph.pb")
-    # classesPath = os.path.join("model_data", "coco.names")
+    # To use the device's local camera, set `use_local_camera` to True.
+    # To use a video stream from a URL, set it to False.
+    use_local_camera = True
+
+    # When using a URL, set the appropriate server address.
+    # This should be a server adress url where the live video stream is being broadcasted not a yt URL.
+    # Example: "http://192.168.1.1:8080/video"
+    server_address = "http://192.168.1.1/" if not use_local_camera else 0
+
+    # configPath = "\\Your\\ssd_mobilenet\\file\\path"
+    # modelPath = "\\Your\\frozen_inference\\file\\path"
+    # classesPath = "\\Your\\coco.names\\file\\path"
+    # "http://192.168.1.1/"
+    configPath = "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+    modelPath = "frozen_inference_graph.pb"
+    classesPath = "coco.names"
     focalLength = 367  # or whatever the correct value is for your setup
 
-    detector = Detector(server_address, configPath, modelPath, classesPath, focalLength)
+    detector = Detector(server_address, configPath, modelPath, classesPath, focalLength, use_local_camera)
     detector.start_processing()
 
     input("Press Enter to stop processing...")
